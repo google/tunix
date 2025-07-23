335174c3b007598ae732f10b8a0cccb49f5087de
Abheesht Sharma <abheesht@google.com>
2025-07-22 22:32:11 -0700

    Internal
    PiperOrigin-RevId: 786138436

--- tunix/rl/utils.py
+++ tunix/rl/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Simple utils used by GRPO."""
+"""Simple utils used by RL algorithms."""
 
 from flax import nnx
 from flax.nnx import statelib
