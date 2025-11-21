b9532550bca93b93e395b78d4146b1237e87b1ef
lc5211 <linchai@google.com>
2025-11-21 23:14:26 +0000

    debug attribute error

--- examples/deepscaler/train_deepscaler_nb.py
+++ examples/deepscaler/train_deepscaler_nb.py
@@ -53,6 +53,16 @@ except wandb.errors.UsageError as e:
   wandb.init(mode="disabled")
 
 
+# from GOOGLE_INTERNAL_PACKAGE_PATH.pyglib import gfile
+# from etils import ecolab
+import optax
+from orbax import checkpoint as ocp
+
+import pathwaysutils
+pathwaysutils.initialize()
+
+print("jax devices: ", jax.devices())
+
 try:
   from etils import ecolab
   cm = ecolab.adhoc(
