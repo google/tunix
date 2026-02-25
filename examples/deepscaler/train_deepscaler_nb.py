f568e576e2b004879f38f3c069b8de4e0445b6ed
lc5211 <linchai@google.com>
2026-02-25 05:39:13 +0000

    update

--- examples/deepscaler/train_deepscaler_nb.py
+++ examples/deepscaler/train_deepscaler_nb.py
@@ -61,20 +61,20 @@ print("jax devices: ", jax.devices())
 # import os
 # os.environ["WANDB_MODE"] = "online"
 
-# try:
-  # wandb.login()
-  # print("linchai: logged in to W&B")
-# except wandb.errors.UsageError as e:
+try:
+  wandb.login()
+  print("linchai: logged in to W&B")
+except wandb.errors.UsageError as e:
   # Handle the error, maybe disable W&B logging
-  # wandb.init(mode="disabled")
+  wandb.init(mode="disabled")
 
 
-# try:
-  # run_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-  # wandb.init(project="tunix", name=run_name, anonymous="allow")
+try:
+  run_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+  wandb.init(project="tunix", name=run_name, anonymous="allow")
   # wandb.init(project="tunix", name=run_name, anonymous="allow", id="lys9lbvw", resume="allow",)
-# except Exception as e:
-  # print(f"linchai: W&B initialization failed with error: {e}")
+except Exception as e:
+  print(f"linchai: W&B initialization failed with error: {e}")
 
 try:
   from etils import ecolab
@@ -621,9 +621,9 @@ cluster_config = rl_cluster_lib.ClusterConfig(
         # so 30000 * 8 = 240000 tokens , given that we have total 2k + 8K = 10k tokens per sample,
         # so effective batch size is 240000 / 10240 = 24 samples per micro batch. num_generations = 8,
         # ideally we can try max to 4. Given we use only 4 devices for trainer, we can set it to 2 here.
-        train_micro_batch_size=1,
+        train_micro_batch_size=2,
         # metrics logging
-        # metrics_logging_options=metrics_logging_options,
+        metrics_logging_options=metrics_logging_options,
         # checkpoint saving
         checkpoint_root_directory=CKPT_DIR,
         checkpointing_options=checkpointing_options,
