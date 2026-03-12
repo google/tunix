a1b56b7a770574d2da41c748cd72ba2555c31f68
Andy Twigg <atwigg@google.com>
2026-03-12 04:27:57 +0000

    allow passing eval_ds parameters through tunix cli, plus bugfixes

--- tunix/models/automodel.py
+++ tunix/models/automodel.py
@@ -292,14 +292,18 @@ def download_model(
       ValueError: If the model source is not supported for downloading.
   """
 
-  if model_source == ModelSource.KAGGLE:
+  if model_source in (ModelSource.KAGGLE, ModelSource.HUGGINGFACE):
     from tunix.oss import utils as oss_utils  # pylint: disable=g-import-not-at-top
 
-    return oss_utils.kaggle_pipeline(model_id_or_path, model_download_path)
-  elif model_source == ModelSource.HUGGINGFACE:
-    from tunix.oss import utils as oss_utils  # pylint: disable=g-import-not-at-top
+    if model_download_path:
+      # Append the model name so different models don't share the same directory.
+      model_name = model_id_or_path.split('/')[-1]
+      model_download_path = os.path.join(model_download_path, model_name)
 
-    return oss_utils.hf_pipeline(model_id_or_path, model_download_path)
+    if model_source == ModelSource.KAGGLE:
+      return oss_utils.kaggle_pipeline(model_id_or_path, model_download_path)
+    else:
+      return oss_utils.hf_pipeline(model_id_or_path, model_download_path)
   elif model_source == ModelSource.GCS:
     return model_id_or_path
   elif model_source == ModelSource.INTERNAL:
