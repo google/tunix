6af3b92318fe61773d3902bbc265942740b4c10e
Shadi Noghabi <noghabi@google.com>
2026-07-28 16:15:08 -0700

    Add FakeWorker for orchestrator tests
    PiperOrigin-RevId: 955528737

---
+++ tests/tunix/experimental/orchestrator/fake_worker_test.py
@@ -0,0 +1,57 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for fake_worker."""
+
+from absl.testing import absltest
+from tunix.tunix.experimental.orchestrator import fake_worker
+
+
+class FakeWorkerTest(absltest.TestCase):
+
+  def test_initial_state(self):
+    worker = fake_worker.FakeWorker()
+    self.assertEqual(worker.state, "CREATED")
+    self.assertEqual(worker.call_counts["initialize"], 0)
+    self.assertEqual(worker.call_counts["compile"], 0)
+    self.assertEqual(worker.call_counts["start"], 0)
+    self.assertEqual(worker.call_counts["stop"], 0)
+
+  def test_initialize(self):
+    worker = fake_worker.FakeWorker()
+    worker.initialize()
+    self.assertEqual(worker.state, "INITIALIZED")
+    self.assertEqual(worker.call_counts["initialize"], 1)
+
+  def test_compile(self):
+    worker = fake_worker.FakeWorker()
+    worker.compile()
+    self.assertEqual(worker.state, "COMPILED")
+    self.assertEqual(worker.call_counts["compile"], 1)
+
+  def test_start(self):
+    worker = fake_worker.FakeWorker()
+    worker.start()
+    self.assertEqual(worker.state, "READY")
+    self.assertEqual(worker.call_counts["start"], 1)
+
+  def test_stop(self):
+    worker = fake_worker.FakeWorker()
+    worker.stop()
+    self.assertEqual(worker.state, "STOPPED")
+    self.assertEqual(worker.call_counts["stop"], 1)
+
+
+if __name__ == "__main__":
+  absltest.main()
