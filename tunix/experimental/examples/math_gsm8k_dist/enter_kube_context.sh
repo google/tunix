00be7c6099ea1e0a0768d2568bb05c0803c3fba4
Yangmu Jiang <yangmu@google.com>
2026-08-24 22:49:11 -0700

    fix: minor fix for gsm8k demo
    PiperOrigin-RevId: 970316656

--- tunix/experimental/examples/math_gsm8k_dist/enter_kube_context.sh
+++ tunix/experimental/examples/math_gsm8k_dist/enter_kube_context.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Usage:
+#   source tunix/experimental/examples/math_gsm8k_dist/enter_kube_context.sh
 
 PROJECT=${PROJECT:-cloud-tpu-multipod-dev}
 REGION=${REGION:-us-central1}
@@ -6,6 +8,6 @@ ZONE=${ZONE:-us-central1-a}
 CLUSTER=${CLUSTER:-trellis-demo-0810}
 
 export KUBECONFIG="${KUBECONFIG:-$HOME/.kube/config.$PROJECT.$REGION.$CLUSTER}"
-gcloud container clusters get-credentials $CLUSTER --region=$REGION --project=$PROJECT --dns-endpoint &>/dev/null || { echo "gcloud get-credentials failed"; exit 1; }
-kubectl config use-context "gke_${PROJECT}_${REGION}_${CLUSTER}" >/dev/null || { echo "kubectl use-context failed"; exit 1; }
-kubectl config set-context --current --namespace=default >/dev/null || { echo "kubectl set-context failed"; exit 1; }
+gcloud container clusters get-credentials $CLUSTER --region=$REGION --project=$PROJECT --dns-endpoint || { echo "gcloud get-credentials failed"; return; }
+kubectl config use-context "gke_${PROJECT}_${REGION}_${CLUSTER}" || { echo "kubectl use-context failed"; return; }
+kubectl config set-context --current --namespace=default || { echo "kubectl set-context failed"; return; }
