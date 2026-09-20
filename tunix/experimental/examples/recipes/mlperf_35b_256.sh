17b58c50973b68bb1ff6e29d39c01b8337724c7e
Andy Twigg <atwigg@google.com>
2026-09-20 05:01:31 +0000

    run 2: v5p-64 trainer, v5p-4 sampler, 16 replicas, full remat

--- tunix/experimental/examples/recipes/mlperf_35b_256.sh
+++ tunix/experimental/examples/recipes/mlperf_35b_256.sh
@@ -55,8 +55,8 @@ export WEIGHT_SYNC_MODE="raiden"
 
 # Topologies (64 chips Trainer 4x4x4, 16x 4-chip Rollout slices)
 export TRAINER_JOBSET_YAML="jobset.pathways.yaml"
-export TRAINER_TPU_SLICE="tpuv5:4x4x8"
-export TRAINER_MESH_FSDP=64
+export TRAINER_TPU_SLICE="tpuv5:4x4x4"
+export TRAINER_MESH_FSDP=32
 export TRAINER_MESH_TP=2
 export TRAINER_MESH_EXPERT=1
 export TRAINER_BASE_NUM_KV_HEADS=2
