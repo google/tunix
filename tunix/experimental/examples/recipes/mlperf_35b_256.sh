f3f8b0e4b9169581e31f1d4ffb3093f60254b3e3
Andy Twigg <atwigg@google.com>
2026-09-23 06:28:32 +0000

    plumb MAX_STALENESS through

--- tunix/experimental/examples/recipes/mlperf_35b_256.sh
+++ tunix/experimental/examples/recipes/mlperf_35b_256.sh
@@ -148,7 +148,7 @@ export NUM_GENERATIONS=16
 export TRAIN_MICRO_BATCH_SIZE="${TRAIN_MICRO_BATCH_SIZE:-32}"
 export CHECKPOINT_SAVE_INTERVAL_STEPS=0
 export CHECKPOINT_MAX_TO_KEEP=10
-export MAX_STALENESS=0
+export MAX_STALENESS=${MAX_STALENESS:-0}
 
 # Sampling Parameters (explicitly disable top-k, set top-p 1.0 and temperature 1.0)
 export TEMPERATURE="1.0"
