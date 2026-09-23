de7017b80359d27a2996181fe7e0e6c4c122700b
Dateng Lin <datenglin@google.com>
2026-09-23 18:04:01 +0000

    refactor(recipes): consolidate Pathways config and merge guide into README
    - Consolidate shared Pathways/Raiden configuration into mlperf_pathways_config.sh and source it in MLPerf recipes.
    - Merge Pathways images and rebuild guide into recipes/README.md and remove PATHWAYS_IMAGES.md.

--- tunix/experimental/examples/recipes/mlperf_35b_256.sh
+++ tunix/experimental/examples/recipes/mlperf_35b_256.sh
@@ -34,19 +34,8 @@ export KUEUE_PRIORITY_CLASS="${KUEUE_PRIORITY_CLASS:-${PRIORITY_CLASS}}"
 export SERVICE_ACCOUNT="xpk-sa"
 export CPU_MACHINE="n2d-standard-64"
 
-# Pathways & Raiden Images and settings (from Google doc)
-export PATHWAYS_SERVER_IMAGE="${PATHWAYS_SERVER_IMAGE:-us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/gke/datenglin/unsanitized_server:raiden_20260923}"
-export PATHWAYS_PROXY_IMAGE="${PATHWAYS_PROXY_IMAGE:-us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/gke/datenglin/unsanitized_proxy_server:raiden_20260923}"
-export PATHWAYS_PROXY_MEMORY_LIMIT="160G"
-export USER_CONTAINER_MEMORY="260G"
-export USER_CONTAINER_MEMORY_LIMIT="${USER_CONTAINER_MEMORY_LIMIT:-260G}"
-export RAIDEN_DEVICES_PER_HOST=4
-export USE_WEIGHT_CONVERTER="true"
-export PREFUSE_MOE_WEIGHTS="true"
-export TRAINER_PREFUSE_MOE_WEIGHTS="true"
-export ROLLOUT_PREFUSE_MOE_WEIGHTS="true"
-export VERIFY_WEIGHTS="true"
-export TRAINER_PADDED_MOE_MLP_DIM=""
+# Pathways & Raiden shared configuration
+source "${DIR}/mlperf_pathways_config.sh"
 
 # WandB configuration
 export WANDB_ENTITY="google-trellis"
--- tunix/experimental/examples/recipes/mlperf_35b_256_v7x.sh
+++ tunix/experimental/examples/recipes/mlperf_35b_256_v7x.sh
@@ -32,19 +32,8 @@ export USE_DYNAMIC_SLICING="true"
 export TPU_RESERVATION="${TPU_RESERVATION:-ghostfish-pogoag4tylwed}"
 export ENABLE_PATHWAYS_PERSISTENCE=1
 
-# Pathways & Raiden Images and settings (from Google doc)
-export PATHWAYS_SERVER_IMAGE="${PATHWAYS_SERVER_IMAGE:-us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/gke/datenglin/unsanitized_server:raiden_20260923}"
-export PATHWAYS_PROXY_IMAGE="${PATHWAYS_PROXY_IMAGE:-us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/gke/datenglin/unsanitized_proxy_server:raiden_20260923}"
-export PATHWAYS_PROXY_MEMORY_LIMIT="160G"
-export USER_CONTAINER_MEMORY="260G"
-export USER_CONTAINER_MEMORY_LIMIT="${USER_CONTAINER_MEMORY_LIMIT:-260G}"
-export RAIDEN_DEVICES_PER_HOST=4
-export USE_WEIGHT_CONVERTER="true"
-export PREFUSE_MOE_WEIGHTS="true"
-export TRAINER_PREFUSE_MOE_WEIGHTS="true"
-export ROLLOUT_PREFUSE_MOE_WEIGHTS="true"
-export VERIFY_WEIGHTS="true"
-export TRAINER_PADDED_MOE_MLP_DIM=""
+# Pathways & Raiden shared configuration
+source "${DIR}/mlperf_pathways_config.sh"
 
 # WandB configuration
 export WANDB_ENTITY="google-trellis"
