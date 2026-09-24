3f12974519d8e6fe19d984bf20ae78433c79b0c6
knightwh <wuhao@google.com>
2026-09-24 23:07:19 +0000

    feat(recipe): enable gdn kernel and tokamax splash for 397b ring cp and gdn remat

--- tunix/experimental/examples/recipes/mlperf_397b_256_v7x.sh
+++ tunix/experimental/examples/recipes/mlperf_397b_256_v7x.sh
@@ -66,6 +66,7 @@ export ROLLOUT_REPLICAS="${ROLLOUT_REPLICAS:-16}"
 export VLLM_ADDITIONAL_CONFIG='{"sharding":{"sharding_strategy":{"expert_parallelism":16,"tensor_parallelism":1,"enable_dp_attention":true}},"custom_mamba_cache_multiplier":16,"maxtext_config":{"scan_layers":false,"attention":"vllm_rpa","allow_split_physical_axes":true,"use_multimodal":false,"prefuse_moe_weights":true,"per_device_batch_size":0.0}}'
 export MAMBA_CACHE_MODE="${MAMBA_CACHE_MODE:-align}"
 export VLLM_MAMBA_CACHE_MODE="${VLLM_MAMBA_CACHE_MODE:-${MAMBA_CACHE_MODE}}"
+export ENABLE_PREFIX_CACHING="${ENABLE_PREFIX_CACHING:-false}"
 
 # Rollout Worker Flags & Raiden tuning
 export ONEHOT_MOE_PERMUTE_THRESHOLD=131072
@@ -96,7 +97,9 @@ gdn_conv=device \
 num_moe_token_chunks=4 \
 moe_chunk_barrier=false \
 use_ring_of_experts=true \
-use_ragged_sort=true}"
+use_ragged_sort=true \
+use_gdn_kernel=true \
+use_tokamax_splash=true}"
 
 # DeepSWE Environment & Agent Sandbox
 export SANDBOX_NODE_SELECTOR_VAL="sandbox-np"
